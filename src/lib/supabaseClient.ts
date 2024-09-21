723525cfa7dec648ebadc67de4abcc150213bcf1
dandele <dandele93@gmail.com>
2024-09-21 15:43:25 +0200

    Primo commit

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yakpcjsjffdyurpebpfn.supabase.co';
-const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlha3BjanNqZmZkeXVycGVicGZuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjg1NTA3MCwiZXhwIjoyMDQyNDMxMDcwfQ.nuJyrNgOw0ga-p288ZPfve3f5_U1haQ6z434bHCLwY4';
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY!;
 
 console.log('Inizializzo Supabase con URL:', supabaseUrl);
 
