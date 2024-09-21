690b701ec168b663ce7b2a3b628fd648c0bca4d3
dandele <dandele93@gmail.com>
2024-09-21 18:40:36 +0200

    primo tentativo di gestione routing avanzato

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,7 @@ export default defineConfig({
     }),
     starlight({
       title: "ScrewFast Docs",
+      disable404Route: "true",  // Disabilita la rotta 404 di Starlight
       defaultLocale: "root",
       locales: {
         root: {
