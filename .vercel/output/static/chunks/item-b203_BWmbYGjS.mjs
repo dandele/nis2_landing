40237fa2315c761bdccc5d28a383a0829c04083b
dandele <dandele93@gmail.com>
2024-09-21 17:31:20 +0200

    ennesimo tentativo di ritorno a node e pulizia per vercel

--- .vercel/output/static/chunks/item-b203_BWmbYGjS.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./item-b203_CR-4CWvh.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- .vercel/output/static/middleware.mjs
+++
@@ -1,4 +0,0 @@
-import 'es-module-lexer';
-import './chunks/shared_Cp2dmJUo.mjs';
-import 'cookie';
-export { onRequest } from './_astro-internal_middleware.mjs';
